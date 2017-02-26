06c337f67713a41802941497cfc4340c8a7da4aa
ryanzhao1993 <ryanzhao1993@qq.com>
2017-02-26 16:34:55 +0800

    commit

--- 3.module/test-person2.js
+++ 3.module/test-person2.js
@@ -3,10 +3,6 @@ var module_person = require('./person.js');  //require是同步的
 console.log(module_person); // { Person: [Function: Person] }
 
 var Person = module_person.Person;
-
+// console.log(Person());
 var person = new Person();
 console.log(person);    // Person { name: 'ryanzhao' }
-
-module_person.Person = function(){
-    console.log('function changed');
-};
